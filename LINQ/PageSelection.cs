ea1f73f570471c5522ddddda13a4daf65f9c1441
Name <email@exaple.ru>
2023-09-06 07:52:07 +0300

    Добавил PageSelection

---
+++ LINQ/PageSelection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class PageSelection
+    {
+        public static char Page = ' ';
+        
+        public static char Selection()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите номер желаемой страницы(1-3): ");
+                if (Page == 1 || Page == 2 || Page == 3)
+                {
+                    Page = Convert.ToChar(Console.ReadKey());
+                    return Page;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Такой страницы нет.");
+                }
+            }
+        }
+    }
+}
